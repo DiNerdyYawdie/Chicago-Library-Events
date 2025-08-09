f8825a913817f5eeca843c3d6bb3d4cc5614c58b
Neikkie <neikkie21@outlook.com>
2025-08-09 09:42:57 -0400

    feat: added list to the event screen

--- Chicago Library Events/Event screen/EventsView.swift
+++ Chicago Library Events/Event screen/EventsView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct EventsView: View {
     var body: some View {
         NavigationStack {
-            
             Text("Event Screen")
                 .navigationTitle(Text("Events"))
-            
+            List{
+                Text("Adult Book Club: Wellness(2023)").font(Font.title.weight(.semibold))
+                Text("Weird Book Club at Midwest Coast Brewing: Mister Magic").font(Font.title.weight(.semibold))
+                Text("Play Day").font(Font.title.weight(.semibold))
+                Text("Adult Book Discussion: Number Go Up: Insude Crypto's Wild Rise and Staggering Fall").font(Font.title.weight(.semibold))
+                Text("Adult Book Discussion: Children of Chicago").font(Font.title.weight(.semibold))
+                Text("Virtual Writing Club / Club de Escritura Virtual").font(Font.title.weight(.semibold))
+                
+            }
         }
     }
 }
