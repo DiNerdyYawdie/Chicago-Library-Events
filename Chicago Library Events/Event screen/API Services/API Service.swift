174faffba20da381bb153f05a0e416dd88083622
Nerdy Yawdie <di_muirhead@hotmail.com>
2025-09-14 10:54:02 -0400

    fix: added comments and updates

--- Chicago Library Events/Event screen/API Services/API Service.swift
+++ Chicago Library Events/Event screen/API Services/API Service.swift
@@ -10,24 +10,35 @@ class ChicagoLibraryEventsServicesImpl: ChicagoLibraryEventsServices {
 	
 	func getChicagoLibraryEvents() async throws -> [LibraryEvent] {
 		
+		// MARK: Declare endpoint for chicago libraries api as a String
 		var endpoint: String = "https://data.cityofchicago.org/api/v3/views/vsdy-d8k7/query.json?app_token=wZcylDAGsAxPdD95RY70L808k"
 		
+		// Convert/Transform `endpoint`, to a type `URL`
+		// To be used to fetch data from the api using `URLSession`
+		/// `guard` means that if it is unable convert the String to URL, then do whats in the else block
 		guard let url: URL = URL(string: endpoint) else {
+			
 			return  []
 		}
 		
 		do {
+			// We use the URL `endpoint` to get the data from the backend
+			// By passing it to `URLSession.shared.data(from: url)`
 			let (data, response) = try await URLSession.shared.data(from: url)
 			
 			guard let httpURLResponse = response as? HTTPURLResponse else {
 				return []
 			}
 			
+			// If the status code in the response is including and between 200-299
+			// Then it is successful
 			guard (200...299).contains(httpURLResponse.statusCode) else {
 				return []
 			}
 			
 			do {
+				// Transforming/ Changing the `data` to and `[LibraryEvent]`
+				// So that we can give it to our `viewModel`
 				let decoder = JSONDecoder()
 				let chicagoLibraryEvents = try decoder.decode([LibraryEvent].self, from: data)
 				return chicagoLibraryEvents
--- Chicago Library Events/Event screen/View Model/EventsScreenViewModel.swift
+++ Chicago Library Events/Event screen/View Model/EventsScreenViewModel.swift
@@ -9,11 +9,14 @@ import Foundation
 
 class EventsScreenViewModel: ObservableObject {
 	
+	// Create a variable to hold the Api Services logic
 	var apiServices: ChicagoLibraryEventsServices = ChicagoLibraryEventsServicesImpl()
 	
+	// Make `libraryEvents` a `@Published` so that the view knows to change, when it gets new data
     @Published var libraryEvents: [LibraryEvent] = []
 	
 	@MainActor
+	// Create a function that we call to get data from the API Service
 	func getChicagoLibraryEvents() async {
 		do {
 			self.libraryEvents = try await apiServices.getChicagoLibraryEvents()
